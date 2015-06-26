99d3a844cd8dfbe95df87e1732db4a2c4d8725a2
Andreas Fischer <gamejam@latticepoint.org>
2015-06-27 08:37:12 +1000

    Implemented smooth player following of camera.

---
+++ Assets/scripts/CameraMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	GameObject player;
+	float camTargetHeight;
+	bool fixedUpdatePerformed = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.Find ("Player");
+
+		camTargetHeight = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (fixedUpdatePerformed)
+		{
+			cameraLagLerp();
+			fixedUpdatePerformed = false;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		fixedUpdatePerformed = true;
+
+	}
+
+	void cameraLagLerp ()
+	{
+		float lerpWeight = 2;
+
+		Vector3 targetPos = player.gameObject.transform.position;
+
+		Vector3 newPos = transform.transform.position;
+
+		// We only want to move in the x, and y dimensions.
+		//targetPos = new Vector3 (targetPos.x, targetPos.y, transform.position.x);
+
+		newPos.x = Mathf.Lerp (transform.position.x, targetPos.x, lerpWeight * Time.deltaTime);
+		newPos.y = Mathf.Lerp (transform.position.y, targetPos.y + camTargetHeight, lerpWeight * Time.deltaTime);
+
+		transform.position = newPos;
+	}
+
+	public void cameraSnapToPlayer ()
+	{
+
+	}
+}
