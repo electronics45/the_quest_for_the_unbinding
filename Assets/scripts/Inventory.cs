74cb4c0fc30c9e373caed94ccf003ba782b2e789
Andreas Fischer <gamejam@latticepoint.org>
2015-06-28 08:52:18 +1000

    Added final "key" assets.

---
+++ Assets/scripts/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+	public Vector3 nextKeyPos;
+
+	public float xSeparation = 0.4;
+
+	List <GameObject> keyBindings;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void drawKeyBinding (KeyBinding binding)
+	{
+		// Spawn prefab. With correct number of keys.
+
+		// Move down to nextKeyPos.
+
+		// Swap texture on keys.
+
+		// Set description text.
+	}
+
+	public void drawAllKeybindings ()
+	{
+
+	}
+
+	public void clearAllKeyBindings ();
+}
