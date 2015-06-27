bc72ffbbcb7932c811575081c2b11d31e7afb010
Rocco <ricotalzier@gmail.com>
2015-06-27 16:00:31 +1000

    first effort at rebind class for key bind menu

---
+++ Assets/scripts/Rebind.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rebind : MonoBehaviour {
+
+	// Use this for initialization
+	public void rebindLeft () {
+
+		foreach (KeyBinding binding in GetComponent <KeyBindings> ().m_keyBindings)
+		{
+			if(binding.m_bindingName == "left")
+			{
+				GetComponent <KeyBindings> ().discardKeyBinding (binding);
+			}
+		}
+
+		if (Event.current.type == EventType.KeyDown) 
+		{
+			KeyCode newBinding = Event.current.keyCode;
+			KeyBinding binding = new KeyBinding ("left", GetComponent <Movement> ());
+			binding.setIsContinuous (true);
+			binding.addKeyDown (newBinding);
+			GetComponent <KeyBindings> ().aquireKeyBinding (binding);
+		}
+	}
+}
